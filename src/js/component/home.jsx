7ee56c8715899f3d3ce8ae1a0796735f12de65d0
LMezza <mezabalza18@gmail.com>
2025-01-02 18:02:41 +0000

    Semaforo primera version

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color1, setColor1] = useState("rojo")
+	const [color2, setColor2] = useState("amarillo")
+	const [color3, setColor3] = useState("verde")
+
+	function encendido1 () {
+		setColor1("brillorojo") 
+		setColor2("amarillo")
+		setColor3("verde")
+	}
+	function encendido2 () {
+		setColor2("brilloamarillo") 
+		setColor1("rojo") 
+		setColor3("verde")
+	}
+	function encendido3 () {
+		setColor3("brilloverde")
+		setColor1("rojo") 
+		setColor2("amarillo")
+	}
+	function apagado () {
+		setColor1("rojo") 
+		setColor2("amarillo")
+		setColor3("verde")
+	}
+	function cambiar() {
+		//console.log("hola");
+		//setTimeout(saludos, 3000);
+		setTimeout(encendido1, 2000);
+		setTimeout(encendido3, 4000);
+		setTimeout(encendido2, 6000);
+		setTimeout(apagado, 8000);
+	}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="">
+			<div className="palo"></div>
+			<div className="semaforo">
+			<div className={color1} onClick={encendido1}></div>
+			<div className={color2} onClick={encendido2}></div>
+			<div className={color3} onClick={encendido3}></div>
+			</div>
+			<button className="boton" type="button" onClick={() => cambiar()}><strong>Change</strong></button>
 		</div>
+		
 	);
 };
 
